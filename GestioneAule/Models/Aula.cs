be879a01fab61a62b726b6945af6447fa5f997a3
Gianfranco Cito <citovich1975@gmail.com>
2026-04-08 11:51:21 +0200

    prima versione del progetto

---
+++ GestioneAule/Models/Aula.cs
@@ -0,0 +1,21 @@
+﻿namespace GestioneAule.Models
+{
+    public class Aula
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Capienza { get; set; }
+
+        public Aula(int id, string nome, int capienza)
+        {
+            Id = id;
+            Nome = nome;
+            Capienza = capienza;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} - Nome: {Nome} - Capienza: {Capienza}";
+        }
+    }
+}
---
+++ GestioneAule/Program.cs
@@ -0,0 +1,5 @@
+﻿using GestioneAule.Models;
+
+Aula aula1 = new Aula(1, "Aula Informatica", 25);
+
+Console.WriteLine(aula1);
