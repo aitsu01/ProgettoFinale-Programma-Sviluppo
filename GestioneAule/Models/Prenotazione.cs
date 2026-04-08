9b0132adef2d703f9944c1678153680c54b9f9af
Gianfranco Cito <citovich1975@gmail.com>
2026-04-08 11:54:49 +0200

    aggiunta classe Prenotazione

---
+++ GestioneAule/Models/Prenotazione.cs
@@ -0,0 +1,29 @@
+﻿namespace GestioneAule.Models
+{
+    public class Prenotazione
+    {
+        public int Id { get; set; }
+        public string NomeCorso { get; set; }
+        public string Docente { get; set; }
+        public int AulaId { get; set; }
+        public DateTime Data { get; set; }
+        public int OraInizio { get; set; }
+        public int OraFine { get; set; }
+
+        public Prenotazione(int id, string nomeCorso, string docente, int aulaId, DateTime data, int oraInizio, int oraFine)
+        {
+            Id = id;
+            NomeCorso = nomeCorso;
+            Docente = docente;
+            AulaId = aulaId;
+            Data = data;
+            OraInizio = oraInizio;
+            OraFine = oraFine;
+        }
+
+        public override string ToString()
+        {
+            return $"Prenotazione {Id} - Corso: {NomeCorso} - Docente: {Docente} - Aula ID: {AulaId} - Data: {Data:dd/MM/yyyy} - {OraInizio}:00-{OraFine}:00";
+        }
+    }
+}
--- GestioneAule/Program.cs
+++ GestioneAule/Program.cs
@@ -1,5 +1,8 @@
 ﻿using GestioneAule.Models;
 
+
 Aula aula1 = new Aula(1, "Aula Informatica", 25);
+Prenotazione pren1 = new Prenotazione(1, "Informatica", "Prof. Rossi", 1, new DateTime(2026, 4, 10), 9, 11);
 
 Console.WriteLine(aula1);
+Console.WriteLine(pren1);
