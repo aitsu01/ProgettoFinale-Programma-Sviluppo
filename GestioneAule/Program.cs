f9d7168cbae91c99eebf9c2b0e38afebbf70c873
Gianfranco Cito <citovich1975@gmail.com>
2026-04-08 12:33:32 +0200

    aggiornato Program.cs con menu prenotazioni aule

--- GestioneAule/Program.cs
+++ GestioneAule/Program.cs
@@ -1,8 +1,92 @@
 ﻿using GestioneAule.Models;
+using GestioneAule.Services;
 
+GestoreAule gestore = new GestoreAule();
+int scelta;
 
-Aula aula1 = new Aula(1, "Aula Informatica", 25);
-Prenotazione pren1 = new Prenotazione(1, "Informatica", "Prof. Rossi", 1, new DateTime(2026, 4, 10), 9, 11);
+do
+{
+    Console.WriteLine("\n--- GESTIONE AULE STUDIO ---");
+    Console.WriteLine("1. Aggiungi aula");
+    Console.WriteLine("2. Mostra aule");
+    Console.WriteLine("3. Prenota aula");
+    Console.WriteLine("4. Mostra prenotazioni");
+    Console.WriteLine("0. Esci");
+    Console.Write("Scelta: ");
 
-Console.WriteLine(aula1);
-Console.WriteLine(pren1);
+    if (!int.TryParse(Console.ReadLine(), out scelta))
+    {
+        Console.WriteLine("Inserisci un numero valido.");
+        continue;
+    }
+
+    switch (scelta)
+    {
+        case 1:
+            Console.Write("ID aula: ");
+            int idAula = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome aula: ");
+            string nomeAula = Console.ReadLine();
+
+            Console.Write("Capienza: ");
+            int capienza = int.Parse(Console.ReadLine());
+
+            gestore.AggiungiAula(new Aula(idAula, nomeAula, capienza));
+            Console.WriteLine("Aula aggiunta con successo.");
+            break;
+
+        case 2:
+            Console.WriteLine("\n--- ELENCO AULE ---");
+            gestore.MostraAule();
+            break;
+
+        case 3:
+            Console.Write("ID prenotazione: ");
+            int idPrenotazione = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome studente: ");
+            string nomeStudente = Console.ReadLine();
+
+            Console.Write("ID aula da prenotare: ");
+            int aulaId = int.Parse(Console.ReadLine());
+
+            Console.Write("Data (yyyy-mm-dd): ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ora inizio (es. 9): ");
+            int oraInizio = int.Parse(Console.ReadLine());
+
+            Console.Write("Ora fine (es. 11): ");
+            int oraFine = int.Parse(Console.ReadLine());
+
+            Prenotazione prenotazione = new Prenotazione(
+                idPrenotazione,
+                nomeStudente,
+                aulaId,
+                data,
+                oraInizio,
+                oraFine
+            );
+
+            if (gestore.AggiungiPrenotazione(prenotazione))
+                Console.WriteLine("Prenotazione aggiunta con successo.");
+            else
+                Console.WriteLine("Errore: aula inesistente, orario non valido o aula già occupata.");
+            break;
+
+        case 4:
+            Console.WriteLine("\n--- ELENCO PRENOTAZIONI ---");
+            gestore.MostraPrenotazioni();
+            break;
+
+        case 0:
+            Console.WriteLine("Uscita dal programma.");
+            break;
+
+        default:
+            Console.WriteLine("Scelta non valida.");
+            break;
+    }
+
+} while (scelta != 0);
---
+++ GestioneAule/Services/GestoreAule.cs
@@ -0,0 +1,79 @@
+﻿using GestioneAule.Models;
+
+namespace GestioneAule.Services
+{
+    public class GestoreAule
+    {
+        private List<Aula> aule = new List<Aula>();
+        private List<Prenotazione> prenotazioni = new List<Prenotazione>();
+
+        public void AggiungiAula(Aula aula)
+        {
+            aule.Add(aula);
+        }
+
+        public void MostraAule()
+        {
+            if (aule.Count == 0)
+            {
+                Console.WriteLine("Nessuna aula presente.");
+                return;
+            }
+
+            foreach (var aula in aule)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+
+        public bool AulaEsiste(int aulaId)
+        {
+            return aule.Any(a => a.Id == aulaId);
+        }
+
+        public bool ControllaDisponibilita(Prenotazione nuovaPrenotazione)
+        {
+            foreach (var prenotazione in prenotazioni)
+            {
+                if (prenotazione.AulaId == nuovaPrenotazione.AulaId &&
+                    prenotazione.Data.Date == nuovaPrenotazione.Data.Date &&
+                    nuovaPrenotazione.OraInizio < prenotazione.OraFine &&
+                    nuovaPrenotazione.OraFine > prenotazione.OraInizio)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool AggiungiPrenotazione(Prenotazione prenotazione)
+        {
+            if (!AulaEsiste(prenotazione.AulaId))
+                return false;
+
+            if (prenotazione.OraInizio >= prenotazione.OraFine)
+                return false;
+
+            if (!ControllaDisponibilita(prenotazione))
+                return false;
+
+            prenotazioni.Add(prenotazione);
+            return true;
+        }
+
+        public void MostraPrenotazioni()
+        {
+            if (prenotazioni.Count == 0)
+            {
+                Console.WriteLine("Nessuna prenotazione presente.");
+                return;
+            }
+
+            foreach (var prenotazione in prenotazioni)
+            {
+                Console.WriteLine(prenotazione);
+            }
+        }
+    }
+}
